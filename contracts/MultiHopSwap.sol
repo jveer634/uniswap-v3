5e346f7bc658b42b5c79344f28dd7a3890ab61ec
jveer634 <jaysparx9@gmail.com>
2024-03-27 15:32:37 +0530

    chore: edit - downgraded OZ and solidity version

--- contracts/MultiHopSwap.sol
+++ contracts/MultiHopSwap.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.7.5;
 pragma abicoder v2;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 contract MultiHopSwap {
     using SafeERC20 for IERC20;
--- contracts/SingleHopSwap.sol
+++ contracts/SingleHopSwap.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.7.5;
 pragma abicoder v2;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 contract SingleHopSwap {
     using SafeERC20 for IERC20;
